a8aa19ce69bb5e81b0b8a2a5bee7c9be3f7979b8
Paul Yuan <paul.yuan@ubitus.net>
2015-03-17 17:41:34 +0800

    modified substring

--- Count Substrings/substring.c
+++ Count Substrings/substring.c
@@ -6,8 +6,8 @@
  */
 
 #include <string.h>
+#include <stdio.h>
 int countSubString(char * str, int max) {
-	int length = strlen(str);
 	int numZeros = 0;
 	int numOnes = 0;
 	char *end = str;
@@ -15,37 +15,68 @@ int countSubString(char * str, int max) {
 	int curCount=0;
 	int total=0;
 
-	while (*end != '\0' && *start != '\0') {
+	for( ;*start!='\0'; start++){
 
-		if (numZeros < max && numOnes < max) {
+		for( ;numZeros < max && numOnes < max && *end!='\0'; end++){
 			if (*end == '0')
 				numZeros++;
 			if (*end == '1')
 				numOnes++;
-			printf("%.*s\n",(int)(end-start+1),  start);
 			curCount++;
-
-			end++;
-			continue;
+		//	printf("%.*s\n",(int)(end-start+1),  start);
 		}
 
-
-		total+=curCount;
-		printf("cur count: %d, total: %d\n", curCount, total);
+		total += curCount;
+	//	printf("cur count: %d, total: %d\n", curCount, total);
 		if (*start == '0')
 			numZeros--;
 		if (*start == '1')
 			numOnes--;
 		curCount--;
-		start++;
 
 
 	}
 
+	return total;
+
+
 }
 
+void readfile(){
+
+    int numProblems, strLength, max, numSubProblems;
+    scanf("%d\n", &numProblems);
+    int problemNum;
+    for(problemNum=1; problemNum<=numProblems; problemNum++){
+
+        scanf("%d %d %d\n", &strLength, &max, &numSubProblems);
+        char str[strLength+1];
+        fgets(str, strLength+1, stdin);
+        str[strLength]='\0';
+        int subProblemi;
+        for (subProblemi=1; subProblemi<=numSubProblems; subProblemi++){
+        	int start, end;
+        	 scanf("%d %d\n", &start, &end );
+        	int length=end-start+1;
+        	char substring[length+1];
+        	strncpy(substring, str+start-1, length);
+        	substring[length]='\0';
+        	int total=countSubString(substring, max);
+        	printf("%d\n", total);
+
+        }
+
+
+
+
+    }
+
+
+}
+
+
 int main() {
-	countSubString("00110011000",  5);
+	readfile();
 	return 0;
 
 }
