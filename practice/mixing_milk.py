deed1b62cd009b25881f93af7675ab5df8f2fcd6
Paul Yuan <paul.yuan@appier.com>
2017-06-26 16:55:16 +0800

    Add mixing milk

--- practice/mixing_milk.py
+++ practice/mixing_milk.py
@@ -33,5 +33,30 @@ SAMPLE INPUT (file milk.in)
 
 '''
 
-def calculate_lowest_milk_purchase_budget(milk_amount, num_farmers, milk_stock_price):
-    
+import time
+
+milk_stock_price = [[5, 20], [9, 40], [3,10], [8, 80], [6,30]]
+
+def calculate_lowest_milk_purchase_budget(milk_amount, milk_stock_price):
+    total_price = 0
+    while milk_amount:
+        min_price_tuple = [10000, 10000]
+        for tuple in milk_stock_price:
+            [price, amount] = tuple
+
+            if price < min_price_tuple[0] and amount > 0:
+                print(tuple)
+                min_price_tuple = tuple
+        [price, amount] = min_price_tuple
+        print(milk_amount, amount)
+        if milk_amount >= amount:
+            total_price += amount * price
+            milk_amount -= amount
+            min_price_tuple[1] = 0
+        else:
+            total_price += milk_amount * price
+            milk_amount = 0
+            min_price_tuple[1] -= milk_amount
+    return total_price
+
+print(calculate_lowest_milk_purchase_budget(120, milk_stock_price))
